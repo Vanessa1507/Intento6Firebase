f66c52879171c3c891175c1eabde6f382e22f095
Vanessa1507 <31253783+Vanessa1507@users.noreply.github.com>
2018-04-13 10:29:32 -0500

    Update FirebaseReferences.java

--- app/src/main/java/com/example/user/intento6firebase/Objetos/FirebaseReferences.java
+++ app/src/main/java/com/example/user/intento6firebase/Objetos/FirebaseReferences.java
@@ -1,8 +1,10 @@
 package com.example.user.intento6firebase.Objetos;
 
 public class FirebaseReferences {
-    final public static String PAIS_REFERENCIA = "tb_paises";
-    final public static String DEPARTAMENTO_REFERENCIA = "tb_departamentos";
+    final public static String TB_PAIS = "tb_paises";
+    final public static String PAIS_REFERENCIA = "PAIS_NOMBRE";
+    final public static String TB_DEPARTAMENTO = "tb_departamentos";
+    final public static String DEPARTAMENTO_REFERENCIA = "DEPARTMENTO_NOMBRE";
     final public static String CIUDAD_REFERENCIA = "tb_ciudades";
 
 }
